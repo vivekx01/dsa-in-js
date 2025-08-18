d623fec29c58e501b41dfaf113f87570070337cb
Vivek Shukla <viveks95944@gmail.com>
2025-08-18 08:58:52 +0530

    more linked list questions

---
+++ 05 linked list/removeDuplicatesFromSortedList.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while(curr && curr.next){
+        if (curr.val == curr.next.val){
+            curr.next = curr.next.next;
+        } 
+        else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};
--- 05 linked list/removeNthNodeFromEnd.js
+++ 05 linked list/removeNthNodeFromEnd.js
@@ -1,5 +1,23 @@
 // https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
+// one pass approach
+var removeNthFromEnd = function(head, n) {
+    let sentinel = new ListNode();
+    sentinel.next = head;
+    let p2 = sentinel;
+    let p1 = sentinel;
+    for (let i = 0; i<n;i++){
+        p1 = p1.next;
+    }
+    while (p1.next){
+        p2 = p2.next;
+        p1 = p1.next;
+    }
+    p2.next = p2.next.next;
+    return sentinel.next;
+};
+
+// two pass approach
 var removeNthFromEnd = function(head, n) {
     let sentinel = new ListNode();
     sentinel.next = head;
